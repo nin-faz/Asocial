18fb1bac19970b2daab1f95dd82e713e4bddc275
Roromain <romainmonmarche@gmail.com>
2025-02-26 11:51:26 +0100

    fixing type

--- back/src/resolvers.ts
+++ back/src/resolvers.ts
@@ -1,7 +1,7 @@
 import { userMutations } from "./domain/user/mutation.js";
 import { Resolvers } from "./types.js";
 
-export const resolvers = {
+export const resolvers: Resolvers = {
     Mutation: {
         ...userMutations
     },

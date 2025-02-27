318866c9dd51d820b8633460b517f66bf41aaa48
nghS <thnghia84@gmail.com>
2025-02-27 18:02:50 +0100

    🚚 : changement nom reference

--- back/src/domain/user/mutation.ts
+++ back/src/domain/user/mutation.ts
@@ -2,7 +2,7 @@ import { MutationResolvers } from "../../types.js";
 import { WithRequired } from "../../utils/mapped-type.js";
 import { createUser } from "./createUser.js";
 import { signIn } from "./signIn.js";
-import { updateUser } from "./updateuser.js";
+import { updateUser } from "./updateUser.js";
 
 type UserMutations = WithRequired<MutationResolvers, 'createUser' | 'signIn'>
 

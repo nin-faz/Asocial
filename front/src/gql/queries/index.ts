74427e7f18729942491d081051edab3198f769d0
Nino <fazernino@gmail.com>
2025-02-28 15:55:35 +0100

    added Display Dislikes

--- front/src/gql/queries/index.ts
+++ front/src/gql/queries/index.ts
@@ -1,7 +1,12 @@
 import { GET_USER_BY_TOKEN } from "./userQueries";
 import { GET_USER_BY_ID } from "./userQueries";
-import { FIND_ARTICLES } from "./articleQuery";
+import { FIND_ARTICLES, FIND_ARTICLE_BY_MOST_DISLIKED } from "./articleQuery";
 import { FIND_DISLIKES_BY_USER_ID } from "./dislikesQuery";
 
-
-export { GET_USER_BY_TOKEN, GET_USER_BY_ID, FIND_ARTICLES, FIND_DISLIKES_BY_USER_ID };
+export {
+  GET_USER_BY_TOKEN,
+  GET_USER_BY_ID,
+  FIND_ARTICLES,
+  FIND_DISLIKES_BY_USER_ID,
+  FIND_ARTICLE_BY_MOST_DISLIKED,
+};
